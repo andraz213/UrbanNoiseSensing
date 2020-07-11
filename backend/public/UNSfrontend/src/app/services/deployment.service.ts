5a56260c4a3af28ddd580d36fb973b2463dd7213
andraz213 <an4858@student.uni-lj.si>
2020-07-11 09:39:20 +0200

    deployment  service got basic functionality

--- backend/public/UNSfrontend/src/app/services/deployment.service.ts
+++ backend/public/UNSfrontend/src/app/services/deployment.service.ts
@@ -1,9 +1,50 @@
+
 import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+
+import { Deployment } from '../models/deployment';
+import {environment} from "../../environments/environment";
+
 
 @Injectable({
   providedIn: 'root'
 })
 export class DeploymentService {
+  private apiUrl = environment.apiUrl;
 
-  constructor() { }
+  constructor(private http: HttpClient,) { }
+
+
+  public getDeployments(): Promise<Deployment[]>{
+    const url = `${this.apiUrl}/deployment`;
+    return this.http
+      .get(url)
+      .toPromise()
+      .then(anwser => anwser as Deployment[])
+      .catch(this.obdelajNapako);
+  }
+
+  public getOneDeployment(id: string): Promise<Deployment[]>{
+    const url = `${this.apiUrl}/deployment/` + id;
+    return this.http
+      .get(url)
+      .toPromise()
+      .then(anwser => anwser as Deployment[])
+      .catch(this.obdelajNapako);
+  }
+
+  public updateDeployment(id: string, body: string): Promise<Deployment[]>{
+    const url = `${this.apiUrl}/deployment/` + id;
+    return this.http
+      .put(url, body)
+      .toPromise()
+      .then(anwser => anwser as Deployment[])
+      .catch(this.obdelajNapako);
+  }
+
+
+  private obdelajNapako(napaka: any): Promise<any> {
+    console.error('Error ', napaka.error.sporočilo || napaka.error.errmsg || napaka);
+    return Promise.reject(napaka.error.sporočilo || napaka.error.errmsg || napaka);
+  }
 }
