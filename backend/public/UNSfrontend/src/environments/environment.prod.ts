2a14462967eb90ebd0a20039186114d0b5bfa408
andraz213 <an4858@student.uni-lj.si>
2020-07-15 13:44:21 +0200

    fixed links

--- backend/public/UNSfrontend/src/environments/environment.prod.ts
+++ backend/public/UNSfrontend/src/environments/environment.prod.ts
@@ -1,3 +1,5 @@
 export const environment = {
-  production: true
+  production: true,
+  apiUrl: "https://urbannoisesensing.herokuapp.com/api",
+  mapsKey: "AIzaSyA9amYWGlkh7IzlVQIlZr4YIjHsWU1fHlA"
 };
