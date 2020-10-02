7cf34a83782a69ee853417c97571caca40f7e800
andraz213 <an4858@student.uni-lj.si>
2020-10-02 11:51:39 +0200

    averages chart in its own component

--- backend/public/UNSfrontend/src/app/components/average-chart/average-chart.component.ts
+++ backend/public/UNSfrontend/src/app/components/average-chart/average-chart.component.ts
@@ -1,4 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, HostListener, Input, OnInit} from '@angular/core';
+import {DataService} from "../../services/data.service";
+import { BrowserModule } from '@angular/platform-browser';
+import { NgxChartsModule } from '@swimlane/ngx-charts';
 
 @Component({
   selector: 'app-average-chart',
@@ -7,9 +10,77 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AverageChartComponent implements OnInit {
 
-  constructor() { }
+  @Input() id: string;
+
+  // options
+  legend: boolean = true;
+  showLabels: boolean = true;
+  animations: boolean = true;
+  xAxis: boolean = true;
+  yAxis: boolean = true;
+  showYAxisLabel: boolean = true;
+  showXAxisLabel: boolean = true;
+  xAxisLabel: string = '';
+  yAxisLabel: string = 'Average decibels';
+  timeline: boolean = true;
+
+  colorScheme = {
+    domain: ['#5AA454', '#E44D25', '#CFC0BB', '#7aa3e5', '#a8385d', '#aae3f5']
+  };
+
+
+  average_data:[any];
+
+  constructor(private dataservice: DataService) { }
 
   ngOnInit(): void {
+    this.getAverages();
+  }
+
+
+  private getAverages(){
+
+    this.dataservice.getAverageDeployment(this.id).then(data =>{
+      let temp_data = {};
+      temp_data["name"] = "averages";
+      temp_data["series"] = [];
+      let ticks = [];
+
+      for(let dd of data){
+        let one = {};
+        one["name"] = (new Date(dd.time)).toLocaleTimeString();
+        one["value"] = dd.average;
+        temp_data["series"].push(one);
+
+      }
+
+      this.average_data = [temp_data];
+      console.log(this.average_data);
+    });
+  }
+
+  scrHeight:any;
+  scrWidth:any;
+
+  @HostListener('window:resize', ['$event'])
+  getScreenSize(event?) {
+    this.scrHeight = window.innerHeight;
+    this.scrWidth = window.innerWidth;
+    console.log(window);
+    console.log(this.scrHeight, this.scrWidth);
+  }
+
+
+  onSelect(data): void {
+    console.log('Item clicked', JSON.parse(JSON.stringify(data)));
+  }
+
+  onActivate(data): void {
+    console.log('Activate', JSON.parse(JSON.stringify(data)));
+  }
+
+  onDeactivate(data): void {
+    console.log('Deactivate', JSON.parse(JSON.stringify(data)));
   }
 
 }
--- backend/public/UNSfrontend/src/app/components/deployment-overview/deployment-overview.component.ts
+++ backend/public/UNSfrontend/src/app/components/deployment-overview/deployment-overview.component.ts
@@ -3,9 +3,6 @@ import {Deployment} from "../../models/deployment";
 import {DeploymentService} from "../../services/deployment.service";
 import {BsModalRef, BsModalService} from "ngx-bootstrap/modal";
 import {Router} from "@angular/router";
-import { BrowserModule } from '@angular/platform-browser';
-import { NgxChartsModule } from '@swimlane/ngx-charts';
-import {DataService} from "../../services/data.service";
 
 @Component({
   selector: 'app-deployment-overview',
@@ -16,127 +13,21 @@ export class DeploymentOverviewComponent implements OnInit {
 
   constructor(private deploymentService: DeploymentService,
               private modalService: BsModalService,
-              private router: Router,
-              private dataservice: DataService) { }
+              private router: Router) { }
 
   @Input() id: string;
   @Input() deployment: Deployment;
   modalRef: BsModalRef;
 
-  view: any[] = [700, 300];
-
-  // options
-  legend: boolean = true;
-  showLabels: boolean = true;
-  animations: boolean = true;
-  xAxis: boolean = true;
-  yAxis: boolean = true;
-  showYAxisLabel: boolean = true;
-  showXAxisLabel: boolean = true;
-  xAxisLabel: string = 'Year';
-  yAxisLabel: string = 'Population';
-  timeline: boolean = true;
-
-  colorScheme = {
-    domain: ['#5AA454', '#E44D25', '#CFC0BB', '#7aa3e5', '#a8385d', '#aae3f5']
-  };
-
-  multi = [
-    {
-      "name": "Germany",
-      "series": [
-        {
-          "name": "1990",
-          "value": 62000000
-        },
-        {
-          "name": "2010",
-          "value": 73000000
-        },
-        {
-          "name": "2011",
-          "value": 89400000
-        }
-      ]
-    },
-
-    {
-      "name": "USA",
-      "series": [
-        {
-          "name": "1990",
-          "value": 250000000
-        },
-        {
-          "name": "2010",
-          "value": 309000000
-        },
-        {
-          "name": "2011",
-          "value": 311000000
-        }
-      ]
-    },
-
-    {
-      "name": "France",
-      "series": [
-        {
-          "name": "1990",
-          "value": 58000000
-        },
-        {
-          "name": "2010",
-          "value": 50000020
-        },
-        {
-          "name": "2011",
-          "value": 58000000
-        }
-      ]
-    },
-    {
-      "name": "UK",
-      "series": [
-        {
-          "name": "1990",
-          "value": 57000000
-        },
-        {
-          "name": "2010",
-          "value": 62000000
-        }
-      ]
-    }
-  ];
 
 
-  average_data:[any];
 
   ngOnInit() {
-    this.getAverages();
 
   }
 
 
-  private getAverages(){
 
-    this.dataservice.getAverageDeployment(this.id).then(data =>{
-      let temp_data = {};
-      temp_data["name"] = "averages";
-      temp_data["series"] = [];
-
-      for(let dd of data){
-        let one = {};
-        one["name"] = dd.time;
-        one["value"] = dd.average;
-        temp_data["series"].push(one);
-      }
-
-      this.average_data = [temp_data];
-      console.log(this.average_data);
-    });
-  }
 
   public finishDeployment(){
     this.deploymentService.finishDeployment(this.deployment._id).then((data) =>{
@@ -161,17 +52,5 @@ export class DeploymentOverviewComponent implements OnInit {
     this.router.navigateByUrl(`deployments`);
   }
 
-
-  onSelect(data): void {
-    console.log('Item clicked', JSON.parse(JSON.stringify(data)));
-  }
-
-  onActivate(data): void {
-    console.log('Activate', JSON.parse(JSON.stringify(data)));
-  }
-
-  onDeactivate(data): void {
-    console.log('Deactivate', JSON.parse(JSON.stringify(data)));
-  }
 
 }
