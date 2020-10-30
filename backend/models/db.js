3379005edb212499a11555ce68ed57da702aad01
andraz213 <an4858@student.uni-lj.si>
2020-10-30 14:53:37 +0100

    url za mongo lol

--- backend/models/db.js
+++ backend/models/db.js
@@ -1,7 +1,11 @@
 const mongoose = require('mongoose');
 require('./models');
 
-const dbURI = 'mongodb+srv://express_api:e2p0eB9bCSdvJhyY@cluster0.ooaot.mongodb.net/noisesensing?retryWrites=true&w=majority';
+// const dbURI = 'mongodb+srv://express_api:e2p0eB9bCSdvJhyY@cluster0.ooaot.mongodb.net/noisesensing?retryWrites=true&w=majority';
+
+const dbURI = 'mongodb+srv://127.0.0.1:27017';
+
+
 mongoose.connect(dbURI, {
     useNewUrlParser: true,
     useCreateIndex: true,
