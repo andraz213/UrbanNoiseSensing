9a59c200d2c4eb7a5ac9119395c9d63576de0c77
Andraž Novak <>
2021-02-16 18:48:15 +0100

    slikce

--- backend/models/db.js
+++ backend/models/db.js
@@ -1,9 +1,9 @@
 const mongoose = require('mongoose');
 require('./models');
 
-//const dbURI = 'mongodb+srv://express_api:e2p0eB9bCSdvJhyY@cluster0.ooaot.mongodb.net/noisesensing?retryWrites=true&w=majority';
+const dbURI = 'mongodb+srv://express_api:e2p0eB9bCSdvJhyY@cluster0.ooaot.mongodb.net/noisesensing?retryWrites=true&w=majority';
 
-const dbURI = 'mongodb://127.0.0.1:27017';
+//const dbURI = 'mongodb://127.0.0.1:27017';
 
 
 mongoose.connect(dbURI, {
